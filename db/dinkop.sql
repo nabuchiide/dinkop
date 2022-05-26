3e6cd3bb53edbf91a2005d42f0b5a7ee7823e691
Hendi Permana <hendi@eluon.com>
2022-05-26 10:39:40 +0700

    uplaod db

--- db/dinkop.sql
+++ db/dinkop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 23 Bulan Mei 2022 pada 05.13
+-- Waktu pembuatan: 26 Bulan Mei 2022 pada 05.38
 -- Versi server: 10.4.21-MariaDB
 -- Versi PHP: 7.4.23
 
@@ -110,7 +110,11 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_name`, `password`, `user_type`, `id`, `no_pegawai`) VALUES
-('winda', 'winda', '1', 3, '1510631170065');
+('winda', 'winda', '1', 3, '1510631170065'),
+('winda bendahara', 'bendahara', '2', 4, '1510631170067'),
+('winda admin', 'admin', '3', 5, '7845123654'),
+('master', 'master', '0', 6, ''),
+('gagal', 'gagal', '4', 7, '');
 
 --
 -- Indexes for dumped tables
@@ -166,7 +170,7 @@ ALTER TABLE `pegawai`
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
